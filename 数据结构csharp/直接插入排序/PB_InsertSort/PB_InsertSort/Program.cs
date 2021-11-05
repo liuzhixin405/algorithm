9e4a998f0b39f4d7c2eb9a7f9cea250f5c7e16f5
liuzhixin405 <164910441@qq.com>
2021-11-05 23:14:37 +0800

    u
    u

--- 数据结构csharp/直接插入排序/PB_InsertSort/PB_InsertSort/Program.cs
+++ 数据结构csharp/直接插入排序/PB_InsertSort/PB_InsertSort/Program.cs
@@ -7,15 +7,19 @@ namespace PB_InsertSort
 {
     class Program
     {
+        /// <summary>
+        /// 第一种
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int[] arr = {23, 45, 16, 7,42};
+            int[] arr = { 23, 45, 16, 7, 42 };
 
             for (int i = 1; i < arr.Length; i++)
             {
                 int curValue = arr[i];
                 int temp = i;
-                while (temp>0 && arr[temp - 1] > curValue)
+                while (temp > 0 && arr[temp - 1] > curValue)
                 {
                     arr[temp] = arr[temp - 1];
                     temp--;
@@ -26,6 +30,25 @@ namespace PB_InsertSort
             foreach (int i in arr)
             {
                 Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        /// 第二种
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="len"></param>
+        static void Insert_Sort(int[] arr, int len)
+        {
+            int i, j;
+            for (i = 1; i < len; i++)
+            {
+                int temp = arr[i];
+                for (j = i - 1; j >= 0 && arr[j] > temp; j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+                arr[j + 1] = temp;
             }
         }
     }
